c6215328bf9b2542dbe96a3c7e2e927a18a850cd
unknown <alemurillo104@gmail.com>
2021-02-10 14:41:18 -0400

    contador en componente

--- Angular/Angular/01-bases/src/app/app.component.ts
+++ Angular/Angular/01-bases/src/app/app.component.ts
@@ -1,23 +1,12 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
 
+
 @Component({
   selector : 'app-root',
   templateUrl : 'app.component.html'
 })
 export class AppComponent {
-  titulo   : string = 'Contador App';
-  contador : number = 0;
-  base     : number = 5;
-
-  acumular (valor : number){
-    this.contador += valor;
-  }
-  incrementar() : void {
-    this.contador++;
-  }
-
-  decrementar() : void {
-    this.contador--;
-  }
+  
+  
 }
